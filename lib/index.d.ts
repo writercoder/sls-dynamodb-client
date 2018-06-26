09ac1004734e6b73c1e162a65ab4f2d5c66f51d0
Kartik Rao <kartik.rao@globalred.com.au>
2018-06-26 14:20:23 +1000

    Updated dist

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,21 +1,21 @@
 import * as AWS from "aws-sdk";
 export declare class DynamoDBClient {
-    localRegionName: string;
+    localRegion: string;
     localEndpoint: string;
-    constructor(localRegionName?: string, localEndpoint?: string);
-    private static applyEnvironment;
+    constructor(localRegion?: string, localEndpoint?: string);
+    private applyEnvironment;
     /**
      * @static getDocumentClient
      * @param options AWS.DynamoDB.DocumentClient.DocumentClientOptions & ServiceConfigurationOptions & AWS.DynamoDB.ClientApiVersions
      * @returns AWS.DynamoDB.DocumentClient
      * @memberof DynamoDBClient
      */
-    static getDocumentClient(options?: any): AWS.DynamoDB.DocumentClient;
+    getDocumentClient(options?: any): AWS.DynamoDB.DocumentClient;
     /**
      * @static getClient
      * @param options AWS.DynamoDB.ClientConfiguration
      * @returns AWS.DynamoDB
      * @memberof DynamoDBClient
      */
-    static getClient(options?: any): AWS.DynamoDB;
+    getClient(options?: any): AWS.DynamoDB;
 }
